1bdad28a67ebc5ab571b079db451123399890adb
cavik <cavik1994@gmail.com>
2019-05-12 23:55:46 +0530

    Add files via upload

---
+++ Controllers/AddUtteranceController.cs
@@ -0,0 +1,32 @@
+﻿using ADBot.Models;
+using ADBot.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ADBot.Controllers
+{
+    public class AddUtteranceController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> GetExampleData(string jsonText)
+        {
+            Utterance utterance = JsonConvert.DeserializeObject<Utterance>(jsonText);
+            ViewBag.SyncOrAsync = "Asynchronous";
+            return Json(await AddUtterances.ApiCall(jsonText), JsonRequestBehavior.AllowGet);
+        }
+    }
+} 
