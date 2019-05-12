aa24fe86f1e99a0c6617478ae9403a9575e76732
cavik <cavik1994@gmail.com>
2019-05-12 23:47:57 +0530

    Add files via upload

---
+++ PublishConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADBot.Models
+{
+    public class PublishConfig
+    {
+        public readonly string versionId; 
+        public readonly Boolean isStaging;
+        public PublishConfig()
+        {
+            this.versionId = "0.2";
+            this.isStaging = false;
+        }
+
+    }
+}
